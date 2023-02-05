7fe64b9b31628ba5630a8fe6bd2700e41186012b
rbhagathsingh <56692823+rbhagathsingh@users.noreply.github.com>
2023-02-05 11:23:37 +0530

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './assets/css/style.scss';
+
+export * from "./components";
+export * from "./shared";
+export * from "./services";
+export * from "./http";
+export * from "./shared/constants";
+export * from "./apiHooks";
+
